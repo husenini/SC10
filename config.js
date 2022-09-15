96c770eadbedcbe77b638d65a0de4523839001fe
husenini <112593176+husenini@users.noreply.github.com>
2022-09-15 14:53:37 +0700

    Update config.js

--- config.js
+++ config.js
@@ -16,16 +16,16 @@ global.xteam = 'apivproject'
 global.lolhuman = 'yourkey'
 
 // Other
-global.owner = ['6281511480762']
-global.premium = ['6281511480762']
-global.ownername = 'YahyaGanzz'
-global.botname = 'AzzBot•MD'
-global.packname = '© YahyaGanzz'
-global.gc = 'https://github.com/AzzBott679'
-global.linkyt = 'https://youtube.com/channel/UCmUhiytK9WMYaOFgdOho_2g'
-global.linkgc = 'https://chat.whatsapp.com/CkguID8hSdSFAaBrKNHLSr'
+global.owner = ['6289510023459']
+global.premium = ['6289510023459']
+global.ownername = 'SenxBot'
+global.botname = 'SenxBot'
+global.packname = '© SenxBot'
+global.gc = ''
+global.linkyt = ''
+global.linkgc = ''
 global.limitawal = '10'
-global.author = '@yahyaganzz\n@AzzBot'
+global.author = '@senlbs\n@SenxBot'
 global.sessionName = 'sessionya'
 global.prefa = ['','!','.','🐦','🐤','🗿']
 global.sp = '⌕'
